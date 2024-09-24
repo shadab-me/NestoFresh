c61fdeb3e89a3d4d980b0b40f60b63d1b7125eb0
shadab ali <shadabali604@gmail.com>
2024-09-24 07:54:35 +0530

    chnaged

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,32 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png"; 
+import { Link, useLocation } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 function Header(props) {
+  const [isSticky, setIsSticky] = useState(false);
+  const location = useLocation();
+
+  // Scroll handler to make header sticky
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Conditionally apply styles based on route
+  const isHomePage = location.pathname === "/";
+
   return (
     <React.Fragment>
-      <header className={classes.header}>
+      <header
+        className={`${
+          isSticky || !isHomePage ? classes.smallHeader : classes.header
+        }`}
+      >
         <div className={classes["header-content"]}>
-          <img src={logo} alt="Nesto Fresh Logo" className={classes.logo} />
-          <div className={classes.buttons}>
-            <button className={classes.btn}>
-              <i className="fas fa-utensils"></i> Food Delivery
-            </button>
-            <button className={classes.btn}>
-              <i className="fas fa-bag"></i> Grocery
-            </button>
-            <button className={classes.btn}>
-              <i className="fas fa-box"></i> Package Delivery
-            </button>
+          <div className={classes.left}>
+            <Link to="/" className={classes.homeButton}>
+              Home
+            </Link>
           </div>
+
+          <div className={classes.right}>
+            <Link to="/cart" className={classes.cartButton}>
+              <HeaderCartButton />
+            </Link>
+          </div>
+
+          {!isSticky && isHomePage && (
+            <>
+              <img src={logo} alt="Nesto Fresh Logo" className={classes.logo} />
+              <div className={classes.buttons}>
+                <button className={classes.btn}>
+                  <i className="fas fa-utensils"></i> Order On Call
+                </button>
+                <button className={classes.btn}>
+                  <i className="fas fa-bag"></i> Order On WhatsApp
+                </button>
+              </div>
+            </>
+          )}
+
+          {(isSticky || !isHomePage) && (
+            <div className={classes.buttons}>
+              <button className={classes.btn}>
+                <i className="fas fa-utensils"></i> Order On Call
+              </button>
+            </div>
+          )}
         </div>
-        <Link to="/cart" className={classes.cartButton}>
-          <HeaderCartButton />
-        </Link>
       </header>
-      <div className={classes["offer-banner"]}>Up to 60% Off!</div>
     </React.Fragment>
   );
 }
